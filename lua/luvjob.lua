e77e778994c5cd7b541445a33ac17a6b4bb1820f
TJ DeVries <devries.timothyj@gmail.com>
2020-05-10 09:15:30 -0400

    Better improvements for reading results

--- lua/luvjob.lua
+++ lua/luvjob.lua
@@ -10,8 +10,25 @@ local function close_safely(handle)
 end
 
 function luvjob:new(o)
-  setmetatable(o, self)
-  return o
+  local obj = {}
+
+  obj.command = o.command
+  obj.args = o.args
+  obj.cwd = o.cwd
+  obj.env = o.env
+  obj.detach = o.detach
+
+  obj._user_on_stdout = o.on_stdout
+  obj._user_on_stderr = o.on_stderr
+  obj._user_on_exit = o.on_exit
+
+  -- Could expose these I suppose
+  obj._raw_stdout = ''
+  obj._raw_stderr = ''
+
+  obj._raw_output = ''
+
+  return setmetatable(obj, self)
 end
 
 function luvjob:send(data)
@@ -34,17 +51,12 @@ function luvjob.shutdown_factory(child)
 end
 
 function luvjob:shutdown(code, signal)
-  if self.on_exit then
-    self:on_exit(code, signal)
+  if self._user_on_exit then
+    self:_user_on_exit(code, signal)
   end
 
-  if self.on_stdout then
-    self.stdout:read_stop()
-  end
-
-  if self.on_stderr then
-    self.stderr:read_stop()
-  end
+  self.stdout:read_stop()
+  self.stderr:read_stop()
 
   self:stop()
 
@@ -84,17 +96,55 @@ end
 function luvjob:start()
   local options = self:create_options()
 
-  self.handle, self.pid = vim.loop.spawn(options.command, options, vim.schedule_wrap(luvjob.shutdown_factory(self)))
+  self.handle, self.pid = vim.loop.spawn(
+    options.command,
+    options,
+    vim.schedule_wrap(luvjob.shutdown_factory(self))
+  )
 
-  if self.on_stdout then
-    self.stdout:read_start(vim.schedule_wrap(self.on_stdout))
-  end
+  self.stdout:read_start(vim.schedule_wrap(function(err, data)
+    if data ~= nil then
+      local subbed = data:gsub("\r", "")
+      self._raw_stdout  = self._raw_stdout .. subbed
+      self._raw_output  = self._raw_output .. subbed
+    end
 
-  if self.on_stderr then
-    self.stderr:read_start(vim.schedule_wrap(self.on_stderr))
-  end
+    if self._user_on_stdout then
+      self._user_on_stdout(err, data)
+    end
+  end))
+
+  self.stderr:read_start(vim.schedule_wrap(function(err, data)
+    if data ~= nil then
+      local subbed = data:gsub("\r", "")
+      self._raw_stderr  = self._raw_stderr .. subbed
+      self._raw_output  = self._raw_output .. subbed
+    end
+
+    if self._user_on_stderr then
+      self._user_on_stderr(err, data)
+    end
+  end))
 
   return self.handle, self.pid
+end
+
+function luvjob:stdout_result()
+  return vim.split(self._raw_stdout, "\n")
+end
+
+function luvjob:stderr_result()
+  return vim.split(self._raw_stderr, "\n")
+end
+
+function luvjob:result()
+  local res = vim.split(self._raw_output, "\n")
+
+  if res[#res] == '' then
+    table.remove(res, #res)
+  end
+
+  return res
 end
 
 function luvjob:pid()
@@ -109,6 +159,81 @@ function luvjob:wait()
 
   while self.handle:is_active() or not self.is_shutdown do
     vim.cmd("sleep 10m")
+  end
+end
+
+function luvjob.accumulate_results(results)
+  return function(err, data)
+    if data == nil then
+      if results[#results] == '' then
+        table.remove(results, #results)
+      end
+
+      return
+    end
+
+    if results[1] == nil then
+      results[1] = ''
+    end
+
+    -- Get rid of pesky \r
+    data = data:gsub("\r", "")
+
+    local line, start, found_newline
+    while true do
+      -- print(data)
+
+      start = string.find(data, "\n") or #data
+      found_newline = string.find(data, "\n")
+
+      line = string.sub(data, 1, start)
+      data = string.sub(data, start + 1, -1)
+
+      line = line:gsub("\r", "")
+      line = line:gsub("\n", "")
+
+      results[#results] = (results[#results] or '') .. line
+
+      if found_newline then
+        table.insert(results, '')
+      else
+        break
+      end
+    end
+
+    -- if found_newline and results[#results] == '' then
+    --   table.remove(results, #results)
+    -- end
+
+    -- if string.find(data, "\n") then
+    --   for _, line in ipairs(vim.fn.split(data, "\n")) do
+    --     line = line:gsub("\n", "")
+    --     line = line:gsub("\r", "")
+
+    --     table.insert(results, line)
+    --   end
+    -- else
+    --   results[#results] = results[#results] .. data
+    -- end
+  end
+end
+
+--- Wait for all jobs to complete
+function luvjob.join(...)
+  local jobs_to_wait = {...}
+
+  while true do
+    if #jobs_to_wait == 0 then
+      break
+    end
+
+    local current_job = jobs_to_wait[1]
+    if current_job.is_shutdown then
+      table.remove(jobs_to_wait, 1)
+    end
+
+    -- vim.cmd.sleep(10)
+    vim.cmd("sleep 100m")
   end
 end
 
